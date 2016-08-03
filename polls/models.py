e0da46c317b955d2f2d31cda5f38f0af1a713894
J0hnCena <joshua99wong@gmail.com>
2016-08-02 21:22:20 -0500

    Add polls to urls, fix indentation, and fix bug where you can no longer compare naive and aware datetimes

--- polls/models.py
+++ polls/models.py
@@ -1,7 +1,7 @@
 import datetime
 
 from django.db import models
-import datetime
+from django.utils import timezone
 
 class Question(models.Model):
 	question_text = models.CharField(max_length=200)	
@@ -9,7 +9,7 @@ class Question(models.Model):
 	def __str__(self):
 		return self.question_text
 	def was_published_recently(self):
-		now = datetime.datetime.now()
+		now = timezone.now()
 		return now - datetime.timedelta(days=1) <= self.pub_date <= now
 	was_published_recently.admin_order_field = 'pub_date'
 	was_published_recently.boolean = True
